89466196a7c4afac15ab2c606b37c6801595fd2d
Karthick V <karthickv@vishwak.com>
2017-08-16 14:25:40 +0530

    Initial Commit

---
+++ PodIgnoreSVN/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PodIgnoreSVN
+//
+//  Created by Sureshbabu Naidu on 16/08/17.
+//  Copyright © 2017 Vishwak Sol pvt. ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
