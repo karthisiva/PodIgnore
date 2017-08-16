c933f362c9c37a5ac982e2b246cb9a1a49f7a530
Karthick V <karthickv@vishwak.com>
2017-08-16 14:57:16 +0530

    Function added

--- PodIgnoreSVN/NewsListViewController.swift
+++ PodIgnoreSVN/NewsListViewController.swift
@@ -15,7 +15,9 @@ class NewsListViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-
+    func sayHello() {
+        print("Hello from NewsListViewController")
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
